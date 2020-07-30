4079097a6890cef6d6e78f0515a444f221ee970f
Saathvik D M <43743162+saathvikdm@users.noreply.github.com>
2020-07-31 02:05:16 +0530

    Add files via upload

---
+++ precache-manifest.e2d06c03afd03407bdd0da4483a9baec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32813df2f5c23a971a8ac3c7b906f01f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4bf86e3aea6684dd833",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "92756a0d73cb73177d46",
+    "url": "/static/css/main.9985740d.chunk.css"
+  },
+  {
+    "revision": "f4bf86e3aea6684dd833",
+    "url": "/static/js/2.577a9910.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.577a9910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92756a0d73cb73177d46",
+    "url": "/static/js/main.b8149cc4.chunk.js"
+  },
+  {
+    "revision": "58e266c1e2c40f47740f",
+    "url": "/static/js/runtime-main.7dce5e48.js"
+  }
+]);
